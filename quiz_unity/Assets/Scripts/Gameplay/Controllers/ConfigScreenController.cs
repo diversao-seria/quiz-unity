53534f4b63d5b0f6dedf248593c02e565b60eea4
Guilherme Dias <31046799+diasgui@users.noreply.github.com>
2021-05-14 14:30:10 -0300

    Adição da tela de configurações
    Adicionando icons e telas de configuração
    
    Configurando text to speech com velocidade de leitura

--- quiz_unity/Assets/Scripts/Gameplay/Controllers/ConfigScreenController.cs
+++ quiz_unity/Assets/Scripts/Gameplay/Controllers/ConfigScreenController.cs
@@ -9,6 +9,7 @@ public class ConfigScreenController : MonoBehaviour
 {
     public Toggle m_screenreader, m_highcontrast;
     public Slider m_slider_screenreader;
+    public Button m_confirmButton, m_reportButton, m_restoreButton;
 
     private void Awake()
     {
@@ -31,6 +32,10 @@ public class ConfigScreenController : MonoBehaviour
         m_slider_screenreader.onValueChanged.AddListener((value) => {
             HandleSliderScreenReader(value);
         });
+
+        m_confirmButton.onClick.AddListener(ReturnToMenu);
+        m_reportButton.onClick.AddListener(ReturnToMenu);
+        m_restoreButton.onClick.AddListener(ReturnToMenu);
     }
 
     void HandleScreenReader(bool value)
@@ -43,16 +48,26 @@ public class ConfigScreenController : MonoBehaviour
     void HandleHighContrast(bool value)
     {
         m_highcontrast.isOn = value;
+        var isOn = value ? "Ativado" : "Desativado";
+        
         AccessibilityController.Instance.SetHighContrastParameter(m_highcontrast.isOn);
         Debug.Log(AccessibilityController.Instance.HIGH_CONTRAST);
+        SpeechController.Instance.StartSpeaking("O Alto contraste está " + isOn);
     }
 
     void HandleSliderScreenReader(float value)
     {
         m_slider_screenreader.value = value;
+
         SpeechController.Instance.Setup(value);
+        SpeechController.Instance.StartSpeaking("A leitura está na velocidade " + value);
 
         // store in memory
         PlayerPrefs.SetFloat("SCREENREADER_SPEED", value);
+    }
+
+    void ReturnToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MenuScreen");
     }
 }
--- quiz_unity/Assets/Scripts/Gameplay/Controllers/MenuScreenController.cs
+++ quiz_unity/Assets/Scripts/Gameplay/Controllers/MenuScreenController.cs
@@ -7,7 +7,7 @@ using TextSpeech;
 
 public class MenuScreenController : MonoBehaviour
 {
-    public Button m_classic, m_survival, m_competition;
+    public Button m_classic, m_survival, m_competition, m_configIcon;
 
     public Image m_profile, m_exit;
 
@@ -26,6 +26,7 @@ public class MenuScreenController : MonoBehaviour
         m_classic.onClick.AddListener(ClassicBehaviour);
         m_survival.onClick.AddListener(SurvivalBehaviour);
         m_competition.onClick.AddListener(CompetitionBehaviour);
+        m_configIcon.onClick.AddListener(ConfigurationBehaviour);
     }
 
     void Update()
@@ -58,6 +59,13 @@ public class MenuScreenController : MonoBehaviour
     {
         SpeechController.Instance.StartSpeaking(m_competition.GetComponentInChildren<Text>().text);
         Debug.Log(" Carregue Competição");
+    }
+
+    void ConfigurationBehaviour()
+    {
+        SpeechController.Instance.StartSpeaking(m_configIcon.GetComponentInChildren<Text>().text);
+        TextToSpeech.Instance.StartSpeak("Configurações");
+        StartCoroutine(TransitionAnimation("ConfigScreen"));
     }
 
     public void InitialExitBehaviour()
