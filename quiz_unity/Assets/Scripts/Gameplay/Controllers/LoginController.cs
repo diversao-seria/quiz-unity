4399cd21111e4b4c348bbc6f8f1d7330ba0c55a1
aKazuo <andrek.yasui@hotmail.com>
2021-10-05 18:39:10 -0300

    Login Script Update

--- quiz_unity/Assets/Scripts/Gameplay/Controllers/LoginController.cs
+++ quiz_unity/Assets/Scripts/Gameplay/Controllers/LoginController.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Networking;
 using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
 
 public class LoginController : MonoBehaviour
 {
     public GameObject signInPanel, loginPanel;
-
+    public Text text;
+    public InputField user, password;
     // Start is called before the first frame update
     void Start()
     {
         signInPanel.SetActive(false);
         loginPanel.SetActive(true);
+
+        //StartCoroutine(LoginUser("teste", "12345"));
     }
 
     // Update is called once per frame
@@ -23,6 +29,7 @@ public class LoginController : MonoBehaviour
     public void LoginButton()
     {
         //Login
+        StartCoroutine(LoginUser(user.text, password.text));
     }
 
     public void SignInButton()
@@ -44,4 +51,43 @@ public class LoginController : MonoBehaviour
         loginPanel.SetActive(true);
 
     }
+
+    IEnumerator LoginUser(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("login", username);
+        form.AddField("password", password);
+
+        string url = "http://ds-quiz.herokuapp.com/authenticate";
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                //Debug.Log(www.error);
+                Debug.Log(www.downloadHandler.text);
+                var value = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(www.downloadHandler.text);
+                foreach (var resp in value)
+                {
+                    Debug.Log(resp.Value["player_authentication"]);
+                    text.text = resp.Value["player_authentication"];
+                }
+            }
+            else
+            {
+                ///Debug.Log(JsonConvert.DeserializeObject<Dictionary<string, string>>(www.downloadHandler.text));
+                var value = JsonConvert.DeserializeObject<Dictionary<string, string>>(www.downloadHandler.text);
+                foreach (KeyValuePair<string, string> resp in value)
+                {
+                    Debug.Log(resp.Key + " " + resp.Value);
+                    text.text += resp.Key + " " + resp.Value + "\n";
+                }
+                Debug.Log(www.downloadHandler.text +  "\n success");
+            }
+        }
+    }
+
+
 }
